d73f65690acfde90c285eb71ce82e10c364f1612
lenovo <ahamroyev04@gmail.com>
2025-09-26 09:37:34 +0500

    solved

--- src/Smallest_Absent_Positive_Greater_Than_Average/Solution.java
+++ src/Smallest_Absent_Positive_Greater_Than_Average/Solution.java
@@ -4,15 +4,15 @@ import java.util.HashSet;
 
 class Solution {
     public int smallestAbsent(int[] nums) {
-        int avg=0;
-        int[] set=new int[101];
-        for(int n:nums){
-            avg+=n;
-            if(n>0) set[n]=1;
+        int avg = 0;
+        int[] set = new int[101];
+        for (int n : nums) {
+            avg += n;
+            if (n > 0) set[n] = 1;
         }
-        avg/=nums.length;
-        for(int i=1;i<=100;i++){
-            if(set[i]==0 && i>avg) return i;
+        avg /= nums.length;
+        for (int i = 1; i <= 100; i++) {
+            if (set[i] == 0 && i > avg) return i;
         }
         return 101;
     }
