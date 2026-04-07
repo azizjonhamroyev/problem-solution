daad8584a78b8de4f9c4a31645aee15e2bf41445
lenovo <ahamroyev04@gmail.com>
2026-04-07 16:29:38 +0500

    solved

---
+++ src/Check_if_Strings_Can_be_Made_Equal_With_Operations_II/Solution.java
@@ -0,0 +1,12 @@
+package Check_if_Strings_Can_be_Made_Equal_With_Operations_II;
+
+import java.util.HashSet;
+
+class Solution {
+    public boolean checkStrings(String s1, String s2) {
+        HashSet<Character> odds = new HashSet<>();
+        HashSet<Character> evens = new HashSet<>();
+
+        return true;
+    }
+}
